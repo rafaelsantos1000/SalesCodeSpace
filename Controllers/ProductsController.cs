9208df7e60228cab97530020b715fd1cd08d5a7c
rafaelsantos1000 <32793270+rafaelsantos1000@users.noreply.github.com>
2024-02-29 10:13:06 +0000

    Edit products

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -104,5 +104,70 @@ namespace SalesCodeSpace.Controllers
             model.Categories = await _combosHelper.GetComboCategoriesAsync();
             return View(model);
         }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Product? product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            EditProductViewModel model = new()
+            {
+                Description = product.Description!,
+                Id = product.Id,
+                Name = product.Name!,
+                Price = product.Price,
+                Stock = product.Stock,
+            };
+
+            return View(model);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, CreateProductViewModel model)
+        {
+            if (id != model.Id)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                Product? product = await _context.Products.FindAsync(model.Id);
+                product!.Description = model.Description;
+                product.Name = model.Name;
+                product.Price = model.Price;
+                product.Stock = model.Stock;
+                _context.Update(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException!.Message.Contains("duplicate"))
+                {
+                    ModelState.AddModelError(string.Empty, "Já existe um produto com o mesmo nome.");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                }
+            }
+            catch (Exception exception)
+            {
+                ModelState.AddModelError(string.Empty, exception.Message);
+            }
+
+            return View(model);
+        }
     }
 }
