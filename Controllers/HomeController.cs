f3073677a606fc3010dde7abbee5c97529e877fe
rafaelsantos1000 <32793270+rafaelsantos1000@users.noreply.github.com>
2024-03-06 14:21:33 +0000

    RBasic home page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SalesCodeSpace.Data;
+using SalesCodeSpace.Data.Entities;
 using SalesCodeSpace.Models;
 
 namespace SalesCodeSpace.Controllers;
@@ -7,16 +10,51 @@ namespace SalesCodeSpace.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly DataContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, DataContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        List<Product> products = await _context.Products
+            .Include(p => p.ProductImages)
+            .Include(p => p.ProductCategories)
+            .OrderBy(p => p.Description)
+            .ToListAsync();
+
+        List<ProductsHomeViewModel> productsHome = new() { new ProductsHomeViewModel() };
+
+        int i = 1;
+        foreach (Product product in products)
+        {
+            if (i == 1)
+            {
+                productsHome.LastOrDefault().Product1 = product;
+            }
+            if (i == 2)
+            {
+                productsHome.LastOrDefault().Product2 = product;
+            }
+            if (i == 3)
+            {
+                productsHome.LastOrDefault().Product3 = product;
+            }
+            if (i == 4)
+            {
+                productsHome.LastOrDefault().Product4 = product;
+                productsHome.Add(new ProductsHomeViewModel());
+                i = 0;
+            }
+            i++;
+        }
+
+        return View(productsHome);
     }
+
 
     public IActionResult Privacy()
     {
