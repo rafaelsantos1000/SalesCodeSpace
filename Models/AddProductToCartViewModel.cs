99eb1572914d5449767e1cbd29ca457cf18d0a25
rafaelsantos1000 <32793270+rafaelsantos1000@users.noreply.github.com>
2024-03-07 15:56:23 +0000

    Show product details and caroussel

---
+++ Models/AddProductToCartViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+using SalesCodeSpace.Data.Entities;
+
+namespace SalesCodeSpace.Models;
+
+public class AddProductToCartViewModel
+{
+    public int Id { get; set; }
+
+    [Display(Name = "Nome")]
+    [MaxLength(50, ErrorMessage = "O campo {0} deve ter no máximo {1} caracteres.")]
+    [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+    public string Name { get; set; }
+
+    [DataType(DataType.MultilineText)]
+    [Display(Name = "Descrição")]
+    [MaxLength(500, ErrorMessage = "O campo {0} deve ter no máximo {1} caracteres.")]
+    public string Description { get; set; }
+
+    [DisplayFormat(DataFormatString = "{0:C2}")]
+    [Display(Name = "Preço")]
+    [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+    public decimal Price { get; set; }
+
+    [DisplayFormat(DataFormatString = "{0:N2}")]
+    [Display(Name = "Stock")]
+    [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+    public float Stock { get; set; }
+
+    [Display(Name = "Categorias")]
+    public string Categories { get; set; }
+
+    public ICollection<ProductImage> ProductImages { get; set; }
+
+    [DisplayFormat(DataFormatString = "{0:N2}")]
+    [Display(Name = "Quantidade")]
+    [Range(0.0000001, float.MaxValue, ErrorMessage = "Deve inserir um valor maior que zero na quantidade.")]
+    [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+    public float Quantity { get; set; }
+
+    [DataType(DataType.MultilineText)]
+    [Display(Name = "Comentários")]
+    public string Remarks { get; set; }
+
+}
